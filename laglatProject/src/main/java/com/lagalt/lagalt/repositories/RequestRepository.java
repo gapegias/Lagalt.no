a8d8781b3365cc5602e591fc94dddd94f78c1c0f
George Pegias <47457023+gapegias@users.noreply.github.com>
2023-03-12 23:42:23 +0200

    Delete laglatProject directory

--- laglatProject/src/main/java/com/lagalt/lagalt/repositories/RequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lagalt.lagalt.repositories;
-
-import com.lagalt.lagalt.models.Request;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RequestRepository extends JpaRepository<Request, Integer> {
-}
