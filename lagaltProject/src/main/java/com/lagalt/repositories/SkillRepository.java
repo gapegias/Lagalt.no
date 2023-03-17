6b31eb6097ed69d2dff39192971fb947261b9f69
George Pegias <47457023+gapegias@users.noreply.github.com>
2023-03-17 11:48:42 +0200

    Delete lagaltProject/src/main/java/com/lagalt directory

--- lagaltProject/src/main/java/com/lagalt/repositories/SkillRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lagalt.repositories;
-
-import com.lagalt.models.Skill;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SkillRepository extends JpaRepository<Skill, Integer> {
-}
