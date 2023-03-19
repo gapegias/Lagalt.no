58ea8e2f7b0b61d422af29493709b1240e45dc1f
George Pegias <47457023+gapegias@users.noreply.github.com>
2023-03-19 11:11:57 +0200

    Update LagaltProjectApplication.java

--- src/main/java/com/lagalt/LagaltProjectApplication.java
+++ src/main/java/com/lagalt/LagaltProjectApplication.java
@@ -2,16 +2,18 @@ package com.lagalt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+*/
 
 @SpringBootApplication
 public class LagaltProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(LagaltProjectApplication.class, args);
     }
-    
+    /*
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -25,5 +27,6 @@ public class LagaltProjectApplication {
             }
         }
     }
+    */
 
 }
