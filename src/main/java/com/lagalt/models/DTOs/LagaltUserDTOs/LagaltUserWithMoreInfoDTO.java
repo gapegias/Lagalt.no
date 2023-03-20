c3ab7a7eaff65cdc3b9e4e23b7bca2e1129a137b
George Pegias <47457023+gapegias@users.noreply.github.com>
2023-03-20 11:41:02 +0200

    Update LagaltUserWithMoreInfoDTO.java

--- src/main/java/com/lagalt/models/DTOs/LagaltUserDTOs/LagaltUserWithMoreInfoDTO.java
+++ src/main/java/com/lagalt/models/DTOs/LagaltUserDTOs/LagaltUserWithMoreInfoDTO.java
@@ -10,15 +10,16 @@ import java.util.Set;
 @Setter
 public class LagaltUserWithMoreInfoDTO {
     // Constructor
-    public LagaltUserWithMoreInfoDTO(int user_id, String user_name, Set<SkillDTO> skills, Set<ProjectDTO> projects){
+    public LagaltUserWithMoreInfoDTO(int user_id, String user_name, String user_about_me, Set<SkillDTO> skills, Set<ProjectDTO> projects){
         this.user_id = user_id;
         this.user_name = user_name;
+        this.user_about_me = user_about_me;
         this.skills = skills;
         this.projects = projects;
     }
     private int user_id;
     private String user_name;
+    private String user_about_me;
     private Set<SkillDTO> skills;
     private Set<ProjectDTO> projects;
-
 }
