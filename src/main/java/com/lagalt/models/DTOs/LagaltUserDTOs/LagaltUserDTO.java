629328b103b5c08f793bd2ddc57b6c9f2ba5b3bc
George Pegias <47457023+gapegias@users.noreply.github.com>
2023-03-22 11:38:17 +0200

    Add files via upload

--- src/main/java/com/lagalt/models/DTOs/LagaltUserDTOs/LagaltUserDTO.java
+++ src/main/java/com/lagalt/models/DTOs/LagaltUserDTOs/LagaltUserDTO.java
@@ -10,14 +10,17 @@ import java.util.Set;
 @Setter
 public class LagaltUserDTO {
     // Constructor
-    public LagaltUserDTO(int user_id, String user_name, String user_about_me, Set<SkillDTO> skills){
+    public LagaltUserDTO(int user_id, String user_name, String user_about_me, boolean user_hide,
+                         Set<SkillDTO> skills){
         this.user_id = user_id;
         this.user_name = user_name;
         this.user_about_me = user_about_me;
+        this.user_hide = user_hide;
         this.skills = skills;
     }
     private int user_id;
     private String user_name;
     private String user_about_me;
+    private boolean user_hide;
     private Set<SkillDTO> skills;
 }
