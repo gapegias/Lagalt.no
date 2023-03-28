f903856f51487dc139d6b72f7ea38f11e7b268a6
George Pegias <47457023+gapegias@users.noreply.github.com>
2023-03-28 14:56:49 +0300

    Add files via upload

---
+++ Frontend/src/Components/Project/ProjectSubmitButton.jsx
@@ -0,0 +1,80 @@
+import { STORAGE_KEY_EDIT_MODE, STORAGE_KEY_PROJECT, STORAGE_KEY_PROJECTS, STORAGE_KEY_PROJECTS_TO_SHOW, STORAGE_KEY_USER } from '../../const/storageKeys'
+import { useUser } from '../../context/UserContext'
+import { useEditMode } from '../../context/EditMode'
+import { updateProject } from '../../api/projects'
+import { useProject } from '../../context/ProjectContext'
+import { useProjects } from '../../context/ProjectsContext'
+import { useProjectsToShow } from '../../context/ProjectsToShowContext'
+import { userByName } from '../../api/user'
+import { storageSave } from '../../utils/storage'
+import { useState } from 'react'
+
+
+const ProjectSubmitButton = ({ updatedProject }) => {
+
+    const { user, setUser } = useUser()
+    const { project, setProject } = useProject()
+    const { projects, setProjects } = useProjects()
+    const { setProjectsToShow } = useProjectsToShow()
+    const { setEditMode } = useEditMode()
+    const [error, setError] = useState(null)
+
+
+    const onSubmit = async () => {
+        if (projects.filter(pr => pr.project_title !== project.project_title)
+            .some(p => p.project_title.toLowerCase() === updatedProject.project_title.toLowerCase())) {
+            alert('The title is already being used, please change it!')
+        }
+        else if (updatedProject.skills.length === 0) {
+            alert('Project should have at least one required skill!')
+        }
+        else {
+            console.log(updatedProject.project_id)
+            const [error, response] = await updateProject(updatedProject)
+            if(error === null){
+
+                let tempProjects = projects.filter(project => project.project_id !== updatedProject.project_id)
+                tempProjects.push(updatedProject)
+                storageSave(STORAGE_KEY_PROJECTS, tempProjects)
+                setProjects(tempProjects)
+    
+                storageSave(STORAGE_KEY_PROJECT, updatedProject)
+                setProject(updatedProject)
+    
+                setProjectsToShow(tempProjects)
+                storageSave(STORAGE_KEY_PROJECTS_TO_SHOW, tempProjects)
+    
+    
+                const [userError, userResponse] = await userByName(user.user_name)
+                if(userError === null){
+
+                    storageSave(STORAGE_KEY_USER, userResponse)
+                    setUser(userResponse)
+                }else{
+                    setError("Action failed. Failed to retrieve user data from database")
+                }
+            }else{
+                setError("Action failed. Project is not updated")
+            }
+
+            storageSave(STORAGE_KEY_EDIT_MODE, false)
+            setEditMode(false)
+        }
+    }
+
+    const onCancel = () => {
+        storageSave(STORAGE_KEY_EDIT_MODE, false)
+        setEditMode(false)
+    }
+
+    return (
+        <>
+            <div className="button-bottom-box">
+                <button className='button-29' type='submit' onClick={onSubmit}>Submit</button>
+                <button className='button-29' type='submit' onClick={onCancel}>Cancel</button>
+            </div>
+            {error !== null && <div>{error}</div>}
+        </>
+    )
+}
+export default ProjectSubmitButton
---
+++ Frontend/src/views/HomePage.jsx
@@ -0,0 +1,140 @@
+import { useEffect, useState } from "react"
+import { findAllMessages } from "../api/messages"
+import { findAllProjects } from "../api/projects"
+import { findAllRequests } from "../api/requests"
+import { findAllSkills } from "../api/skills"
+import { loginUser} from "../api/user"
+import NavBar from "../Components/NavBar/NavBar"
+import ProjectSet from "../Components/Project/ProjectSet"
+import TopicsBar from "../Components/TopicsBar/TopicsBar"
+import { ROLES } from "../const/roles"
+import { STORAGE_KEY_EDIT_MODE, STORAGE_KEY_MESSAGES, STORAGE_KEY_PROJECTS, STORAGE_KEY_PROJECTS_TO_SHOW, STORAGE_KEY_REQUESTS, STORAGE_KEY_SKILLS, STORAGE_KEY_USER, STORAGE_KEY_USERS, STORAGE_KEY_USER_PROFILE } from "../const/storageKeys"
+import { useEditMode } from "../context/EditMode"
+import { useMessages } from "../context/MessagesContext"
+import { useProjects } from "../context/ProjectsContext"
+import { useProjectsToShow } from "../context/ProjectsToShowContext"
+import { useRequests } from "../context/RequestsContext"
+import { useSkills } from "../context/SkillsContext"
+import { useUser } from "../context/UserContext"
+import { useUserProfile } from "../context/UserProfileContext"
+import keycloak from "../keycloak"
+import { storageDelete, storageSave } from "../utils/storage"
+import './HomePage.css'
+
+
+const HomePage = () => {
+
+    const { projects, setProjects } = useProjects()
+    const { projectsToShow, setProjectsToShow } = useProjectsToShow();
+    const { requests, setRequests } = useRequests()
+    const { messages, setMessages } = useMessages()
+    const { skills, setSkills } = useSkills()
+    const { setEditMode } = useEditMode()
+    const { setUser } = useUser()
+    const { setUserProfile } = useUserProfile()
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    // Fetching all needed data from API and saves them to the session storage
+    const getAllData = async () => {
+        setIsLoading(true)
+        const [projectErrorApi, projectsResponse] = await findAllProjects()
+        if(projectErrorApi !== null){
+            setError('Failed to retrieve projects from database')
+            return
+        }
+        let sortedProjects = projectsResponse.map(pr => pr).sort(function (x, y) { return y.project_id - x.project_id })
+        storageSave(STORAGE_KEY_PROJECTS, sortedProjects)
+        storageSave(STORAGE_KEY_PROJECTS_TO_SHOW, sortedProjects)
+        setProjects(sortedProjects)
+        setProjectsToShow(sortedProjects)
+
+        const [messagesErrorApi, messagesResponse] = await findAllMessages()
+        if(messagesErrorApi !== null){
+            setError('Failed to retrieve messages from database')
+            return
+        }
+        storageSave(STORAGE_KEY_MESSAGES, messagesResponse)
+        setMessages(messagesResponse)
+
+        const [skillsErrorApi, skillsResponse] = await findAllSkills()
+        if(skillsErrorApi !== null){
+            setError('Failed to retrieve skills from database')
+            return
+        }
+        storageSave(STORAGE_KEY_SKILLS, skillsResponse)
+        setSkills(skillsResponse)
+
+        const [requestsErrorApi, requestsResponse] = await findAllRequests()
+        if(requestsErrorApi !== null){
+            setError('Failed to retrieve requests from database')
+            return
+        }
+        storageSave(STORAGE_KEY_REQUESTS, requestsResponse)
+        setRequests(requestsResponse)
+
+        setIsLoading(false)
+    }
+
+    const readUser = async () => {
+        if (typeof keycloak.tokenParsed !== 'undefined') {
+            const [error, userResponse] = await loginUser(keycloak.tokenParsed.preferred_username)
+            if(error !== null){
+                setError('Failed to retrieve user from database')
+                return
+            }
+            storageSave(STORAGE_KEY_USER, userResponse)
+            storageSave(STORAGE_KEY_USER_PROFILE, userResponse)                  // saves user's data to the session storage
+            setUser(userResponse)
+            setUserProfile(userResponse)
+        }
+    }
+
+    const deleteUser = ()=>{
+        storageDelete(STORAGE_KEY_USER)
+        setUser(null)
+        storageDelete(STORAGE_KEY_USER_PROFILE)
+        setUserProfile(null)
+    }
+
+    const areDataFetched = () => {
+        if (projects.length > 0 &&
+            requests.length > 0 &&
+            messages.length > 0 &&
+            skills.length > 0) {
+            return true
+        }
+        return false
+    }
+
+    useEffect(() => {
+        if (projects.length === 0) {
+            getAllData()
+        }
+        storageSave(STORAGE_KEY_EDIT_MODE, false)
+        setEditMode(false)
+        if (keycloak.hasRealmRole(ROLES.User)) {
+            readUser()
+        } else {
+            deleteUser()
+        }
+    }, []);
+
+
+    return (
+        <>
+            <NavBar />
+            <div className="Main">
+                <div className="Side-bar">
+                    {!isLoading && areDataFetched() && <TopicsBar />}
+                </div>
+                <div className="Main-body">
+                    {isLoading && <h2>Loading Page</h2>}
+                    {!isLoading && areDataFetched() && <ProjectSet projectsToDisplay={projectsToShow} />}
+                </div>
+                {error !== null && <div>{error}</div>}
+            </div>
+        </>
+    )
+}
+export default HomePage
